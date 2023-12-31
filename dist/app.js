dccebb21ab176d126b0aad7b74e25bb3a72f50a5
진찬호 <chanhojin94@kakao.com>
2023-12-31 17:00:33 +0900

    inheritance, class

--- dist/app.js
+++ dist/app.js
@@ -24,22 +24,62 @@
 //   throw { message, errorCode: code };
 // }
 // generateError("An error occured!", 500);
-let name1;
-function add() {
-    name1 = "chano";
-    return name1;
+// let name1;
+// function add() {
+//   name1 = "chano";
+//   return name1;
+// }
+// console.log(add());
+// console.log("asdf");
+// const hobbies = ["reading a book", "watching a movie", "walking and running"];
+// const newHobbies: string[] = [];
+// newHobbies.push(...hobbies);
+// console.log(newHobbies);
+// const person = {
+//   name: "chano",
+//   age: 29,
+// };
+// const copiedPerson = person;
+// copiedPerson.name = "juno";
+// console.log(copiedPerson);
+// console.log(person);
+// const [person1, person2] = ["chano", "juno"];
+// console.log(person2);
+// const [hobby1, hobby2, ...rest] = hobbies;
+// console.log(hobby2);
+class Department {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        // public name: string;
+        this.employees = [];
+        // this.name = name;
+    }
+    describe() {
+        console.log(`Department: ${this.name} : (${this.id})`);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
-console.log(add());
-console.log("asdf");
-const hobbies = ["reading a book", "watching a movie", "walking and running"];
-const newHobbies = [];
-newHobbies.push(...hobbies);
-console.log(newHobbies);
-const person = {
-    name: "chano",
-    age: 29,
-};
-const copiedPerson = person;
-copiedPerson.name = "juno";
-console.log(copiedPerson);
-console.log(person);
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super(id, "d1");
+        this.admins = [];
+        this.admins = admins;
+    }
+}
+const its = new ITDepartment("dfd", ["Max"]);
+its.addEmployee("Max");
+its.addEmployee("Manu");
+// its.employees[2] = "Anna";
+its.name = "management";
+its.describe();
+its.printEmployeeInformation();
+console.log(its);
+// const copiedAccounting = { name: "accounting", describe: accounting.describe };
+// copiedAccounting.describe();
--- src/app.ts
+++ src/app.ts
@@ -30,26 +30,79 @@
 
 // generateError("An error occured!", 500);
 
-let name1;
-function add() {
-  name1 = "chano";
-  return name1;
+// let name1;
+// function add() {
+//   name1 = "chano";
+//   return name1;
+// }
+
+// console.log(add());
+// console.log("asdf");
+
+// const hobbies = ["reading a book", "watching a movie", "walking and running"];
+// const newHobbies: string[] = [];
+// newHobbies.push(...hobbies);
+// console.log(newHobbies);
+
+// const person = {
+//   name: "chano",
+//   age: 29,
+// };
+
+// const copiedPerson = person;
+// copiedPerson.name = "juno";
+// console.log(copiedPerson);
+// console.log(person);
+
+// const [person1, person2] = ["chano", "juno"];
+// console.log(person2);
+
+// const [hobby1, hobby2, ...rest] = hobbies;
+
+// console.log(hobby2);
+
+class Department {
+  // public name: string;
+  private employees: string[] = [];
+  constructor(private readonly id: string, public name: string) {
+    // this.name = name;
+  }
+
+  describe(this: Department) {
+    console.log(`Department: ${this.name} : (${this.id})`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
-console.log(add());
-console.log("asdf");
+class ITDepartment extends Department {
+  admins: string[] = [];
+  constructor(id: string, admins: string[]) {
+    super(id, "d1");
+    this.admins = admins;
+  }
+}
 
-const hobbies = ["reading a book", "watching a movie", "walking and running"];
-const newHobbies: string[] = [];
-newHobbies.push(...hobbies);
-console.log(newHobbies);
+const its = new ITDepartment("dfd", ["Max"]);
 
-const person = {
-  name: "chano",
-  age: 29,
-};
+its.addEmployee("Max");
+its.addEmployee("Manu");
 
-const copiedPerson = person;
-copiedPerson.name = "juno";
-console.log(copiedPerson);
-console.log(person);
+// its.employees[2] = "Anna";
+its.name = "management";
+
+its.describe();
+its.printEmployeeInformation();
+
+console.log(its);
+
+// const copiedAccounting = { name: "accounting", describe: accounting.describe };
+
+// copiedAccounting.describe();
