fd27d0fd7bc2809ae33bee0112c3dd63a2cc9a6c
진찬호 <chanhojin94@kakao.com>
2023-12-31 22:11:17 +0900

    generic

--- dist/app.js
+++ dist/app.js
@@ -2,3 +2,19 @@
 const athletics = [];
 athletics.push("Tennis", 5);
 console.log(athletics);
+function merge(objA, obj2) {
+    return Object.assign(objA, obj2);
+}
+const userA = {
+    name: "chano",
+    age: 40,
+    gender: "male",
+    hobby: "reading",
+};
+const user2 = {
+    name: "juno",
+    age: 39,
+    gender: "female",
+};
+const mergedObj = merge(userA, user2);
+mergedObj.gender;
--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,24 @@ const athletics: Array<String | Number> = [];
 athletics.push("Tennis", 5);
 
 console.log(athletics);
+
+function merge<T, U>(objA: T, obj2: U) {
+  return Object.assign(objA, obj2);
+}
+
+const userA = {
+  name: "chano",
+  age: 40,
+  gender: "male",
+  hobby: "reading",
+};
+
+const user2 = {
+  name: "juno",
+  age: 39,
+  gender: "female",
+};
+
+const mergedObj = merge(userA, user2);
+
+mergedObj.gender;
